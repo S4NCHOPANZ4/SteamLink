fe8d583d30d315a09e324a50a3793ea9abfef56d
Juan Buitrago <buitr4go@gmail.com>
2023-09-23 01:37:00 -0700

    roulette fixed

--- client/src/components/Roulete.tsx
+++ client/src/components/Roulete.tsx
@@ -10,7 +10,7 @@ const Roulete = ({ data }: props) => {
   const divRef = useRef<HTMLDivElement | null>(null);
 
   const [clase, setClase] = useState<boolean>(false);
-  const [dataa, setData] = useState<CSgoWeaponSkin[]>(data);
+  const [dataList, setData] = useState<CSgoWeaponSkin[]>(data);
 
   function shuffleArray(array: CSgoWeaponSkin[]) {
 
@@ -26,54 +26,39 @@ const Roulete = ({ data }: props) => {
   }
 
   useEffect(() => {
-
       setData(shuffleArray(data));
-
-
   }, []);
 
-  useEffect(() => {
-    if (divRef.current) {
-      const width = divRef.current.offsetWidth;
-      console.log(width / 2);
 
-      // Añadimos lógica para calcular el valor de x en la transformación CSS
-      const translateX = `translate(-50%, 0)`;
-
-      setAnchoDelDiv(width / 2);
-      setTransform(translateX); // Usamos setTransform para actualizar la transformación
-    }
-  }, []);
-
-  const [anchoDelDiv, setAnchoDelDiv] = useState<number | null>(null);
-  const [transform, setTransform] = useState<string>(""); // Estado para la transformación CSS
-
-  const spin = () => {
-    // Tu lógica para girar la ruleta aquí
-  };
   // data={(caseData.contains.length % 2 === 0)? caseData.contains : caseData.contains.slice(0, -1)}
 
   return (
     <>
-      <div className="bg-green-500 h-[100px] w-[600px] m-auto  overflow-hidden relative">
-        <div
-          ref={divRef}
-          style={clase ? { transform: transform,
-          transition: "all 1000ms ease-out",
-          left: "50%"
-          } : {}}
-          className={`h-full flex  left-0 absolute `}
-        >
-          {dataa.map((item, i: number) => {
+      <div className="bg-green-500 h-[100px] max-w-[600px] m-auto overflow-hidden relative">
+        <div 
+        style={
+          clase?
+          {
+            transform: "translate(-50%, 0)",
+            transition: "all 1000ms ease-out",
+            left: "50%",
+            width: `${(dataList.length+1) * 100}px`, 
+          }
+          :
+          {}
+        }
+        className="flex  h-full absolute bg-orange-400 ">
+          {dataList.map((item, i: number) => {
             return (
+
               <img
-                className={`h-full w-[120px] ${
-                  i === Math.floor(dataa.length / 2)
+                className={`h-full w-[100px]  ${
+                  i === Math.floor(dataList.length / 2)
                     ? "bg-red-700"
                     : "bg-gray-50"
                 }`}
                 src={item.image}
-                alt=""
+                alt="asd"
                 key={i}
               />
             );
--- client/src/pages/CasePage.tsx
+++ client/src/pages/CasePage.tsx
@@ -49,38 +49,10 @@ const CasePage = () => {
       </div>
 
       {caseData ?
-            <Roulete 
-            data={[
-              {id: 'skin-852832', name: 'Galil AR | Blue Titanium', rarity: 'Mil-Spec Grade', paint_index: '216', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4264e8ec1cedb3bce31a89c934c630bd56d480c1.png'},
-{id: 'skin-197500', name: 'Five-SeveN | Nightshade', rarity: 'Mil-Spec Grade', paint_index: '223', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4f94edf6ac032c15fedafa6ba1f2425a4ca8bded.png'},
-{id: 'skin-1704832', name: 'PP-Bizon | Water Sigil', rarity: 'Mil-Spec Grade', paint_index: '224', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.c4d0aad9cb87870f84f3709ff3b8ecc9fe489d89.png'},
-{id: 'skin-2294660', name: 'Nova | Ghost Camo', rarity: 'Mil-Spec Grade', paint_index: '225', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.d63e06a38ef396a945defd7c1ad9da9ba79fe60d.png'},
-{id: 'skin-721812', name: 'G3SG1 | Azure Zebra', rarity: 'Mil-Spec Grade', paint_index: '229', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.42199f9c4c0bd1ff9f569d6e7348c5cedf4630f5.png'},{id: 'skin-2360216', name: 'P250 | Steel Disruption', rarity: 'Mil-Spec Grade', paint_index: '230', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.36220b384099ed61c2b9ba64af899b892e4a4c58.png'} ,{id: 'skin-459656', name: 'AK-47 | Blue Laminate', rarity: 'Restricted', paint_index: '226', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.9f8ec5103efa5b7a0b3919a13b4de78e1bd612a0.png'}
-,{id: 'skin-1246096', name: 'P90 | Blind Spot', rarity: 'Restricted', paint_index: '228', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a702b8feaf0e2568e60efb2ab722819b0521ea4b.png'},
-{id: 'skin-655976', name: 'FAMAS | Afterimage', rarity: 'Classified', paint_index: '154', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.5d45539735d838ffefd26467a8c767b71807c785.png'},
-{id: 'skin-590732', name: 'AWP | Electric Hive', rarity: 'Classified', paint_index: '227', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.2189958e2afa2c69cfcdd807124c279a0e645625.png'} 
-,{id: 'skin-66460', name: 'Desert Eagle | Cobalt Disruption', rarity: 'Classified', paint_index: '231', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a486db3160bcdcf6bc5a1d8179c450b02f620151.png'},
-{id: 'skin-852832', name: 'Galil AR | Blue Titanium', rarity: 'Mil-Spec Grade', paint_index: '216', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4264e8ec1cedb3bce31a89c934c630bd56d480c1.png'},
-{id: 'skin-197500', name: 'Five-SeveN | Nightshade', rarity: 'Mil-Spec Grade', paint_index: '223', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4f94edf6ac032c15fedafa6ba1f2425a4ca8bded.png'},
-{id: 'skin-1704832', name: 'PP-Bizon | Water Sigil', rarity: 'Mil-Spec Grade', paint_index: '224', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.c4d0aad9cb87870f84f3709ff3b8ecc9fe489d89.png'},
-{id: 'skin-2294660', name: 'Nova | Ghost Camo', rarity: 'Mil-Spec Grade', paint_index: '225', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.d63e06a38ef396a945defd7c1ad9da9ba79fe60d.png'},
-{id: 'skin-721812', name: 'G3SG1 | Azure Zebra', rarity: 'Mil-Spec Grade', paint_index: '229', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.42199f9c4c0bd1ff9f569d6e7348c5cedf4630f5.png'},{id: 'skin-2360216', name: 'P250 | Steel Disruption', rarity: 'Mil-Spec Grade', paint_index: '230', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.36220b384099ed61c2b9ba64af899b892e4a4c58.png'} ,{id: 'skin-459656', name: 'AK-47 | Blue Laminate', rarity: 'Restricted', paint_index: '226', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.9f8ec5103efa5b7a0b3919a13b4de78e1bd612a0.png'}
-,{id: 'skin-1246096', name: 'P90 | Blind Spot', rarity: 'Restricted', paint_index: '228', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a702b8feaf0e2568e60efb2ab722819b0521ea4b.png'},
-{id: 'skin-655976', name: 'FAMAS | Afterimage', rarity: 'Classified', paint_index: '154', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.5d45539735d838ffefd26467a8c767b71807c785.png'},
-{id: 'skin-590732', name: 'AWP | Electric Hive', rarity: 'Classified', paint_index: '227', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.2189958e2afa2c69cfcdd807124c279a0e645625.png'} 
-,{id: 'skin-66460', name: 'Desert Eagle | Cobalt Disruption', rarity: 'Classified', paint_index: '231', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a486db3160bcdcf6bc5a1d8179c450b02f620151.png'},
-{id: 'skin-852832', name: 'Galil AR | Blue Titanium', rarity: 'Mil-Spec Grade', paint_index: '216', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4264e8ec1cedb3bce31a89c934c630bd56d480c1.png'},
-{id: 'skin-197500', name: 'Five-SeveN | Nightshade', rarity: 'Mil-Spec Grade', paint_index: '223', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4f94edf6ac032c15fedafa6ba1f2425a4ca8bded.png'},
-{id: 'skin-1704832', name: 'PP-Bizon | Water Sigil', rarity: 'Mil-Spec Grade', paint_index: '224', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.c4d0aad9cb87870f84f3709ff3b8ecc9fe489d89.png'},
-{id: 'skin-2294660', name: 'Nova | Ghost Camo', rarity: 'Mil-Spec Grade', paint_index: '225', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.d63e06a38ef396a945defd7c1ad9da9ba79fe60d.png'},
-{id: 'skin-721812', name: 'G3SG1 | Azure Zebra', rarity: 'Mil-Spec Grade', paint_index: '229', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.42199f9c4c0bd1ff9f569d6e7348c5cedf4630f5.png'},{id: 'skin-2360216', name: 'P250 | Steel Disruption', rarity: 'Mil-Spec Grade', paint_index: '230', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.36220b384099ed61c2b9ba64af899b892e4a4c58.png'} ,{id: 'skin-459656', name: 'AK-47 | Blue Laminate', rarity: 'Restricted', paint_index: '226', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.9f8ec5103efa5b7a0b3919a13b4de78e1bd612a0.png'}
-,{id: 'skin-1246096', name: 'P90 | Blind Spot', rarity: 'Restricted', paint_index: '228', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a702b8feaf0e2568e60efb2ab722819b0521ea4b.png'},
-{id: 'skin-655976', name: 'FAMAS | Afterimage', rarity: 'Classified', paint_index: '154', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.5d45539735d838ffefd26467a8c767b71807c785.png'},
-{id: 'skin-590732', name: 'AWP | Electric Hive', rarity: 'Classified', paint_index: '227', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.2189958e2afa2c69cfcdd807124c279a0e645625.png'} 
-,{id: 'skin-66460', name: 'Desert Eagle | Cobalt Disruption', rarity: 'Classified', paint_index: '231', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a486db3160bcdcf6bc5a1d8179c450b02f620151.png'}
-            ]}
-            /> :
-            <></>
+        <Roulete
+          data={(caseData.contains.length % 2 === 0)? caseData.contains : caseData.contains.slice(0, -1)}
+        /> :
+        <></>
       }
 
       {/* {caseData? 
@@ -98,3 +70,13 @@ const CasePage = () => {
 }
 
 export default CasePage
+
+// {id: 'skin-852832', name: 'Galil AR | Blue Titanium', rarity: 'Mil-Spec Grade', paint_index: '216', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4264e8ec1cedb3bce31a89c934c630bd56d480c1.png'},
+// {id: 'skin-197500', name: 'Five-SeveN | Nightshade', rarity: 'Mil-Spec Grade', paint_index: '223', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.4f94edf6ac032c15fedafa6ba1f2425a4ca8bded.png'},
+// {id: 'skin-1704832', name: 'PP-Bizon | Water Sigil', rarity: 'Mil-Spec Grade', paint_index: '224', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.c4d0aad9cb87870f84f3709ff3b8ecc9fe489d89.png'},
+// {id: 'skin-2294660', name: 'Nova | Ghost Camo', rarity: 'Mil-Spec Grade', paint_index: '225', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.d63e06a38ef396a945defd7c1ad9da9ba79fe60d.png'},
+// {id: 'skin-721812', name: 'G3SG1 | Azure Zebra', rarity: 'Mil-Spec Grade', paint_index: '229', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.42199f9c4c0bd1ff9f569d6e7348c5cedf4630f5.png'},{id: 'skin-2360216', name: 'P250 | Steel Disruption', rarity: 'Mil-Spec Grade', paint_index: '230', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.36220b384099ed61c2b9ba64af899b892e4a4c58.png'} ,{id: 'skin-459656', name: 'AK-47 | Blue Laminate', rarity: 'Restricted', paint_index: '226', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.9f8ec5103efa5b7a0b3919a13b4de78e1bd612a0.png'}
+// ,{id: 'skin-1246096', name: 'P90 | Blind Spot', rarity: 'Restricted', paint_index: '228', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a702b8feaf0e2568e60efb2ab722819b0521ea4b.png'},
+// {id: 'skin-655976', name: 'FAMAS | Afterimage', rarity: 'Classified', paint_index: '154', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.5d45539735d838ffefd26467a8c767b71807c785.png'},
+// {id: 'skin-590732', name: 'AWP | Electric Hive', rarity: 'Classified', paint_index: '227', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.2189958e2afa2c69cfcdd807124c279a0e645625.png'}
+// ,{id: 'skin-66460', name: 'Desert Eagle | Cobalt Disruption', rarity: 'Classified', paint_index: '231', image: 'https://steamcdn-a.akamaihd.net/apps/730/icons/eco…arge.a486db3160bcdcf6bc5a1d8179c450b02f620151.png'},
