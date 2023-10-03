356739b470f01c82e09870d7ccdb8ffa272480f9
Juan Buitrago <buitr4go@gmail.com>
2023-10-03 14:29:20 -0700

    deploy server url

--- server/routes/csgoDb-routes.js
+++ server/routes/csgoDb-routes.js
@@ -6,8 +6,7 @@ const ErrorHandler = require("../utils/ErrorHandler.js");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors.js");
 const steamUrls = require("../SteamUrl.js");
 const axios = require("axios");
-const redis = require('redis');
-const util = require('util');
+
 
 //==================fetch cs item================
 
@@ -187,49 +186,33 @@ router.get(
 );
 
 
-router.post('/data/getPrice', async (req, res) => {
-  const { item } = req.body;
+router.post("/data/getPrice",
+  catchAsyncErrors(async (req, res, next) => {
+    const { item } = req.body 
+    try {
+      const response = await axios.get(
+          `https://csgobackpack.net/api/GetItemPrice/?currency=USD&id=${item}&time=7&icon=1`
+        );
+      if(response.data.success){
+        res.status(201).json({
+          success: true,
+          data: response.data,
+        });
+      }else{
+        res.status(404).json({
+          success: false,
+          data: {
+            user: 'NotFound err 404 chech api http://csgobackpack.net/api/GetItemPrice'
+          },
+        });
+      }
+      
+    } catch (error) {
+      return next(new ErrorHandler(error.message , 500));
 
-  try {
-    // Intentar obtener datos de la caché
-    const cachedData = await util.promisify(redisClient.get).bind(redisClient)(item);
-    if (cachedData) {
-      const parsedData = JSON.parse(cachedData);
-      res.status(201).json({
-        success: true,
-        data: parsedData,
-      });
-    } else {
-      // Si no hay datos en caché, realizar la solicitud a la API
-      const response = await axios.get(`https://csgobackpack.net/api/GetItemPrice/?currency=USD&id=${item}&time=7&icon=1`);
-
-      // Almacenar la respuesta en caché por un tiempo determinado (por ejemplo, 10 minutos)
-      redisClient.setex(item, 600, JSON.stringify(response.data));
-
-      // Enviar la respuesta al cliente con la estructura deseada
-      res.status(201).json({
-        success: true,
-        data: response.data,
-      });
     }
-  } catch (error) {
-    console.error('Error al obtener datos del usuario:', error);
-    if (error.response && error.response.status === 500) {
-      res.status(500).json({
-        success: false,
-        error: 'Internal Server Error',
-        errorMessage: 'Error en la API externa. Por favor, inténtalo de nuevo más tarde.',
-      });
-    } else {
-      res.status(500).json({
-        success: false,
-        error: 'Internal Server Error',
-        errorMessage: error.message,
-      });
-    }
-  }
-});
-
+  })
+);
 router.get("/data/agents",
   catchAsyncErrors(async (req, res, next) => {
     try {
