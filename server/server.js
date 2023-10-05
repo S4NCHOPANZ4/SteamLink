6f0ebbc378415d262a501a54fb83c0ed7e6dc395
Juan Buitrago <buitr4go@gmail.com>
2023-10-04 18:58:01 -0700

    fixed /data/getPrice

--- server/server.js
+++ server/server.js
@@ -45,12 +45,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use('/assets/data/getPrice', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://csgobackpack.net/api/GetItemPrice');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 
 const authRoutes = require('./routes/passport-routes');
 const csgoGameAssets = require('./routes/csgoDb-routes');
